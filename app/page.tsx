5ce6969cbe94a4cd0e2549f6f72d4b8cad05da2a
secrete-party <secrete-party@petracula.com>
2025-08-11 10:53:48 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
           <div className="grid md:grid-cols-3 gap-12">
             <div className="text-center">
               <div className="text-4xl font-bold text-purple-500 mb-4">1</div>
-              <h3 className="text-xl font-bold mb-4">Let's Level Up</h3>
+              <h3 className="text-xl font-bold mb-4">Time for a Suite</h3>
               <p className="text-gray-300 leading-relaxed">
                 It's time that our crew got a suite. We don't need to commit to doing art the whole time,
                 but Trash Chef proved that we have fun taking some time to schtick. Let's level up.
@@ -67,26 +67,24 @@ export default function Home() {
 
             <div className="text-center">
               <div className="text-4xl font-bold text-purple-500 mb-4">2</div>
-              <h3 className="text-xl font-bold mb-4">Donate!</h3>
+              <h3 className="text-xl font-bold mb-4">Donate</h3>
               <p className="text-gray-300 leading-relaxed">
-                We're told suites will probably cost a little over than $3,000 this year.
-                
-                If you are down to donate, put in your name and Venmo account, BC will  
+                Put in your info and we'll  
                 Venmo request your contribution (unless it's milk or placenta, which you can't venmo yet). 
-                If there is enough interest in executing this plan (as demonstrated by donations), I'll try to book a
-                suite for our team. If there is not enough money in the pot by the time of the second ticket sale, or if
-                we are not able to book, a suite everyone's money will be returned. If we end up with more contributions
-                than we need, BC will return any extra money on a pro-rated basis (don't worry I know math).
+                If we have enough committed in time for either ticket sale, we'll try to book a
+                suite for our team. If there is not enough interest or if
+                we are not able to get a suite, everyone's money will be returned. If we end up with more contributions
+                than we need, we will return any extra money on a pro-rated basis.
               </p>
             </div>
 
             <div className="text-center">
               <div className="text-4xl font-bold text-purple-500 mb-4">3</div>
-              <h3 className="text-xl font-bold mb-4">Enjoy the Benefits!</h3>
+              <h3 className="text-xl font-bold mb-4">Enjoy the Benefits</h3>
               <p className="text-gray-300 leading-relaxed">
-              
-                We'll either just leave the doors unlocked or give the whole squad key cards so we can come and go as we please,
-              and we can do a little or as much schtick hosting as we want.
+                We can do as little or as much schtick hosting as we want, and 
+                we'll either just leave the doors unlocked or give the whole squad keys so we can come and go as we please.
+            
               </p>
             </div>
           </div>
